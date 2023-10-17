d75bb64f4cca857773ed0c0c0b162e130cdd0ace
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-10-17 21:23:00 +0600

    created api

--- index.js
+++ index.js
@@ -23,12 +23,36 @@ const client = new MongoClient(uri, {
   }
 });
 
+// server side data 
+const brands = [
+  {id : 1 , brandName : "Apple" , 
+  imageLink : "https://www.slashgear.com/img/gallery/the-real-meaning-behind-apples-brand-name/l-intro-1650830667.jpg"},
+
+  {id : 2 , brandName : "Samsung" , 
+  imageLink : "https://www.simplilearn.com/ice9/free_resources_article_thumb/How_Samsung_Marketing_Strategy_Solidifies_Its_Brand_Value.jpg"},
+
+  {id : 3 , brandName : "Sony" , 
+  imageLink : "https://variety.com/wp-content/uploads/2016/07/sony-corp-usa-building-placeholder-logo.jpg"},
+
+  {id : 4 , brandName : "Google" , 
+  imageLink : "https://www.verdict.co.uk/wp-content/uploads/2018/09/shutterstock_1160472559-e1536065238946.jpg"},
+
+  {id : 5 , brandName : "Intel" , 
+  imageLink : "https://kenyanwallstreet.com/wp-content/uploads/2023/06/2J8DPRY.jpg"},
+
+  {id : 6 , brandName : "Xiaomi" , 
+  imageLink : "https://www.smartprix.com/bytes/wp-content/uploads/2021/09/lithuania-throws-out-xiaomi-phones-heres-why-R3zUQVnx.jpg"}
+
+]
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    app.get('/brands' , (req , res) =>{
+      res.send(brands)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
